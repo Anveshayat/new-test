80cb79e6c7da4071e45f1d2d557d2c18641af1f8
vardhan <kokkiligaddavardhan0000@gmail.com>
2024-06-05 12:58:56 +0530

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test_github.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
